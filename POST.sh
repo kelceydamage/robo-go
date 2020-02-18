3a736313a55f5a0a0939b5e51267ca62ca194f2e
Kelcey Jamison-Damage <kdamage@palantir.com>
2020-02-18 13:28:10 -0800

    Tue Feb 18 13:28:04 PST 2020 - auto commit

--- POST.sh
+++ POST.sh
@@ -1,6 +1,7 @@
 RED='\033[0;31m'
 GREEN='\033[0;92m'
 ORANGE='\033[38;2;255;165;0m'
+BLUE='\033[0;94m'
 NC='\033[0m'
 
 printMessage() {
@@ -17,13 +18,14 @@ handleError () {
 }
 
 DATE=`date`
+printMessage "${BLUE}INFO:${NC} Starting post operation"
 ./godelw format
 ./godelw license
 ./godelw check
 handleError $? "Can't post due to errors. Please review ${ORANGE}./godelw check${NC}"
 ./godelw test
 handleError $? "Can't post due to errors. Please review ${ORANGE}./godelw test${NC}"
-printMessage "${GRREN}SUCCESS:${NC} All checks passed"
+printMessage "${GREEN}SUCCESS:${NC} All checks passed"
 git add .
 git config --global user.name "Kelcey Jamison-Damage"
 git commit -m "$DATE - auto commit"
